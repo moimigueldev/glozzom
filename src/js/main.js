570c93cb6c62e7998da99eeb3c6606eabcb1026c
Moises Miguel <moiesmigueledu@gmail.com>
2018-03-14 14:07:33 -0500

    finished the about us section

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,9 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$(".slider").slick({
+  infinite: true,
+  slideToShow:1,
+  slideToScroll:1
+});
